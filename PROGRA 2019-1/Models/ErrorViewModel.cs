34103a39bba61522e4938f1d7c802505cd797aae
AxelChavez12 <48738281+AxelChavez12@users.noreply.github.com>
2019-05-08 23:01:40 -0500

    productos

---
+++ PROGRA 2019-1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PROGRA_2019_1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
